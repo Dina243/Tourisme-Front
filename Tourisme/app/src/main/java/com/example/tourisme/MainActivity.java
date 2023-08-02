c232ec2e45c57327cbf04e5c776521bdcca40dc0
Omega243 <140796147+Dina243@users.noreply.github.com>
2023-08-02 10:01:27 +0300

    Initiation splash screen + Updates

--- Tourisme/app/src/main/java/com/example/tourisme/MainActivity.java
+++ Tourisme/app/src/main/java/com/example/tourisme/MainActivity.java
@@ -2,6 +2,12 @@ package com.example.tourisme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+/*
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+*/
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Chargez le Fragment du Splash Screen au démarrage de l'activité principale
+        //loadFragment(new SplashScreenFragment());
     }
+
+    // Méthode pour charger un fragment dans le conteneur
+    //private void loadFragment(Fragment fragment) {
+        //FragmentManager fragmentManager = getSupportFragmentManager();
+        //FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        //fragmentTransaction.replace(R.id.fragment_container, fragment);
+        //fragmentTransaction.commit();
+    //}
 }
