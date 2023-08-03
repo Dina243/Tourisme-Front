7d0ac594e471266c5692563d9a9f8e32a30f0645
Omega243 <140796147+Dina243@users.noreply.github.com>
2023-08-03 10:09:31 +0300

    new updates / correction des erreurs

--- Tourisme/app/src/main/java/com/example/tourisme/MainActivity.java
+++ Tourisme/app/src/main/java/com/example/tourisme/MainActivity.java
@@ -1,6 +1,6 @@
 package com.example.tourisme;
-
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+//import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 /*
 import androidx.appcompat.app.AppCompatActivity;
